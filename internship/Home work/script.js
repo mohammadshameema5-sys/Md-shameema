7292be9bb54d7d85a53e2e1f933ddcf33dc7c328
mohammadshameema5-sys <mohammadshameema5@gmail.com>
2025-12-24 13:05:59 +0530

    done

---
+++ internship/Home work/script.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>DOM</title>
+    </head>
+<body>
+    <h1>DOM</h1>
+    <script>
+        alert("Welcome");
+        prompt("enter your name:");
+    </script>
+</body>
+</html>
